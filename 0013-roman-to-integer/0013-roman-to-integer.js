8a600dd17009bb301b66d713f2a1d381a7ae07ec
unknown <wisnuyeahfaizal@gmail.com>
2024-07-22 10:26:25 +0700

    Time: 108 ms (74.80%), Space: 53.9 MB (91.08%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -4,22 +4,22 @@
  */
 var romanToInt = function (s) {
   const h = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
   };
 
   let val = 0;
 
   for (let i = 0; i < s.length; i++) {
-    if (i === 0 || h[s[i]] <= h[s[i - 1]]) {
-      val += h[s[i]];
+    if (h[s[i]] < h[s[i + 1]]) {
+      val -= h[s[i]];
     } else {
-      val += h[s[i]] - h[s[i - 1]] * 2;
+      val += h[s[i]];
     }
   }
 
