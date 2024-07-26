6bbae22602d735a4df6b3ba85cfffeb0ed7b3cd8
Wisnuyasha Faizal <wisnuyeahfaizal@gmail.com>
2024-07-26 17:50:14 +0700

    Time: 51 ms (93.08%), Space: 50.9 MB (88.61%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,17 +3,29 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let h = {}
+//     let h = {}
+//     for(let i = 0; i < nums.length; i++){
+//         if(nums[i] in h) {
+//             h[nums[i]] += 1 
+//             if(h[nums[i]] >= (nums.length / 2)) {
+//                 return nums[i]
+//             }
+//         } else {
+//             h[nums[i]] = 1
+//         }
+//     }
+    
+//     return nums[0]
+    
+    let candidate = 0;
+    let count = 0;
+    
     for(let i = 0; i < nums.length; i++){
-        if(nums[i] in h) {
-            h[nums[i]] += 1 
-            if(h[nums[i]] >= (nums.length / 2)) {
-                return nums[i]
-            }
-        } else {
-            h[nums[i]] = 1
+        if(count === 0) {
+            candidate = nums[i]
         }
+        count += (nums[i] === candidate) ? 1 : -1;
     }
     
-    return nums[0]
+    return candidate
 };
