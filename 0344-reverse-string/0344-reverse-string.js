06980a9b9d4e5bc3f86749dacee7a99dfeb90f0c
Wisnuyasha Faizal <wisnuyeahfaizal@gmail.com>
2024-08-05 16:11:38 +0700

    Time: 81 ms (44.94%), Space: 55.9 MB (92.16%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -5,14 +5,6 @@
 var reverseString = function(s) {
     let j = s.length - 1
     let i = 0
-    // for(let i = 0; i < s.length; i++) {
-    //     if(i >= (s.length / 2)) {
-    //         break
-    //     } else {
-    //         [s[i], s[j]] = [s[j], s[i]]
-    //         j--
-    //     }
-    // }
     while(i <= j) {
         [s[i], s[j]] = [s[j], s[i]]
         i++
