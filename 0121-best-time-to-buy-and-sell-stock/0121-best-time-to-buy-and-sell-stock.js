3abb1cdf01062add93559d8ac693c9d65580c19d
Wisnuyasha Faizal <wisnuyeahfaizal@gmail.com>
2024-08-06 16:17:55 +0700

    Time: 69 ms (80.83%), Space: 58.9 MB (58.89%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,43 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
-    // let buy = 0
     let maxProfit = 0
-    let minSell = prices[0]
+    let minPrice = prices[0]
     
-//     while(buy < (prices.length - 1)) {
-//         let sell = buy + 1
-//         while(sell < prices.length){
-//             profit = prices[sell] - prices[buy]
-//             if(prices[sell] > prices[buy] && profit > maxProfit){
-//                 maxProfit = profit
-//             }
-//             sell++
-//         }
-        
-//         buy++
-//     }
-    
-    for(let i = 0; i < prices.length; i++) {
-//         prices[i] === minSell ---> continue
-        
-//         prices[i] < minSell ---> minSell = prices[i]
-        
-//         prices[i] > minSell  ---> 
-//             prices[i] - minSell > maxProfit ---> maxProfit = prices[i] - minSell
-//             prices[i] - minSell < maxProfit ---> continue
-    
-        if(prices[i] === minSell) continue
-        
-        if(prices[i] < minSell) minSell = prices[i]
-        
-        if(prices[i] > minSell) {
-            let profit = prices[i] - minSell
+    for(let i = 0; i < prices.length; i++) {        
+        if(prices[i] < minPrice) minPrice = prices[i]
+        else {
+            let profit = prices[i] - minPrice
             if(profit > maxProfit) {
                 maxProfit = profit
-            } else {
-                continue
             }
         }
     }
