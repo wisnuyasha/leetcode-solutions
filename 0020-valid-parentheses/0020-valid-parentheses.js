0d645bfc1fc37f437b282559629b201514334996
Wisnuyasha Faizal <wisnuyeahfaizal@gmail.com>
2024-08-07 17:43:20 +0700

    Time: 44 ms (98.32%), Space: 49.9 MB (66.13%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -4,65 +4,21 @@
  */
 var isValid = function(s) {
     let stack = []
-    // const arr = s.split("")
-    
-//     while(i < s.length) {
-//         if ((s[i] === "{" && s[i + 1] === "}") || 
-//             (s[i] === "(" && s[i + 1] === ")") || 
-//             (s[i] === "[" && s[i + 1] === "]")) {
-//             i += 2;
-//             continue;
-//         }
-        
-//         if ((s[i] === "{" && (s[i + 1] === "]" || s[i + 1] === ")")) || 
-//             (s[i] === "[" && (s[i + 1] === "}" || s[i + 1] === ")")) || 
-//             (s[i] === "(" && (s[i + 1] === "}" || s[i + 1] === "]"))) {
-//             console.log(`Unexpected closing bracket at position ${i + 1}`);
-//         }            
-//         i += 2
-//     }
-    
-    // return true
-    
-    const valid = {
+    const closing = {
         '}': '{',
         ']': '[',
         ')': '('
     }
     
     for(let char of s) {
-        if(char === "(" || char === "{" || char === "[") {
-            stack.push(char)
-        } else {
-            if(stack.length == 0 || stack.pop() !== valid[char]) {
+        if(char in closing) {
+            if(stack.length == 0 || stack.pop() !== closing[char]) {
                 return false
             }
+        } else {
+            stack.push(char)
         }
     }
     
-    if(stack.length === 0) {
-        return true
-    } else {
-        return false
-    }
-    
-//     for(let i = 0; i < s.length; i++) {
-//         if(s[i] === "(" && s[i] === "{"  && s[i] === "[") {
-//             stack.push(s[i])
-//         }
-        
-//         if(s[i] === ")" && arr[0] === "(" ) {
-//             stack.pop()
-//         } else if() {
-//             return false
-//         }
-        
-//         if(){
-//            return false
-//         }
-        
-//         return true
-//     }
-    
-    // if()
+    return stack.length === 0
 };
