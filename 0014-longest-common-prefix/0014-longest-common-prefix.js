012f647266f1c5f39935efd9b077f4f2a41b7443
Wisnuyasha Faizal <wisnuyeahfaizal@gmail.com>
2024-08-08 17:16:25 +0700

    Time: 48 ms (88.41%), Space: 49.2 MB (68.29%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -6,17 +6,18 @@ var longestCommonPrefix = function(strs) {
     if(strs.length == 0) return ""
     
     let ans = ""
-    for(let i = 0; i < strs[0].length; i++){
-        let j = 1
-        let char = strs[0].charAt(i);
-        while(j < strs.length){
-            if(i >= strs[j].length || strs[j].charAt(i) !== char) {
-                return ans
-            }
-            j++
+    strs.sort()
+    
+    const fs = strs[0];
+    const ls = strs[strs.length - 1];
+    
+    for(let i = 0; i < strs[0].length; i++) {
+        if(fs.charAt(i) == ls.charAt(i)) {
+           ans += strs[0].charAt(i)
+        } else {
+            return ans
         }
-        ans += char
-    }
+    } 
      
     return ans
 };
