b047d837d42ff150378dbf3b01f26fb0666af944
unknown <wisnuyeahfaizal@gmail.com>
2024-08-16 18:13:35 +0700

    Time: 60 ms (89.73%), Space: 51 MB (81.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,32 +7,14 @@ var isAnagram = function(s, t) {
     if(s.length !== t.length) return false
     
     let h = {}
-    
-//     const sString = s.split("").sort().join("")
-//     const tString = t.split("").sort().join("")
-        
-//     for(let i = 0; i < sString.length; i++) {
-//         if(sString.charAt(i) !== tString.charAt(i)) return false
-//     }
-    
-    
-    for(let i = 0; i < s.length; i++) {
-        if(s.charAt(i) in h ) {
-            h[s.charAt(i)] += 1
-        } else {
-            h[s.charAt(i)] = 1
-        }
+
+    for (let i = 0; i < s.length; i++) {
+        h[s[i]] = (h[s[i]] || 0) + 1;
     }
-        
-    for(let i = 0; i < s.length; i++) {
-        if(t.charAt(i) in h) {
-            if(h[t.charAt(i)] == 0) {
-                return false
-            }
-            h[t.charAt(i)] -= 1
-        } else {
-            return false
-        }
+    
+      for (let i = 0; i < t.length; i++) {
+        if (!h[t[i]]) return false;
+        h[t[i]]--;
     }
     
     return true
