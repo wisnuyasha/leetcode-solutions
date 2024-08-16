051b22629cf7e243379b1989bd22dab55c2fc3d6
Wisnuyasha Faizal <wisnuyeahfaizal@gmail.com>
2024-08-16 17:12:58 +0700

    Time: 69 ms (62.87%), Space: 50.5 MB (94.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,13 +4,35 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    const sString = s.split("").sort().join("")
-    const tString = t.split("").sort().join("")
+    if(s.length !== t.length) return false
     
-    if(sString.length !== tString.length) return false
+    let h = {}
     
-    for(let i = 0; i < sString.length; i++) {
-        if(sString.charAt(i) !== tString.charAt(i)) return false
+//     const sString = s.split("").sort().join("")
+//     const tString = t.split("").sort().join("")
+        
+//     for(let i = 0; i < sString.length; i++) {
+//         if(sString.charAt(i) !== tString.charAt(i)) return false
+//     }
+    
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s.charAt(i) in h ) {
+            h[s.charAt(i)] += 1
+        } else {
+            h[s.charAt(i)] = 1
+        }
+    }
+        
+    for(let i = 0; i < s.length; i++) {
+        if(t.charAt(i) in h) {
+            if(h[t.charAt(i)] == 0) {
+                return false
+            }
+            h[t.charAt(i)] -= 1
+        } else {
+            return false
+        }
     }
     
     return true
