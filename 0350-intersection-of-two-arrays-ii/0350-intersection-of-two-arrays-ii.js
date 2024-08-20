d2960bc11bd6d119eb5cc812d2fd8774cd87adfb
Wisnuyasha Faizal <wisnuyeahfaizal@gmail.com>
2024-08-20 18:14:33 +0700

    Time: 55 ms (71.82%), Space: 50.3 MB (76.42%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -4,7 +4,6 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    // swap if nums1 is bigger     
     if (nums1.length > nums2.length) return intersect(nums2, nums1);
 
     let h = {}
@@ -13,13 +12,10 @@ var intersect = function(nums1, nums2) {
     for(let i = 0; i < nums1.length; i++){
         h[nums1[i]] = (h[nums1[i]] || 0) + 1
     }
-    
-    console.log(h)
-    
+        
     for(let j = 0; j < nums2.length; j++){
         if(h[nums2[j]] > 0) {
             arr.push(nums2[j])
-            console.log("same")
             h[nums2[j]] -= 1
         }
     }
