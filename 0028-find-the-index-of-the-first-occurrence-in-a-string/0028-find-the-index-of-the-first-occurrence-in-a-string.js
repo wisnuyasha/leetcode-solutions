cb8b8c2a4df94190ebdbe563bac8c27570fa9d47
Wisnuyasha Faizal <wisnuyeahfaizal@gmail.com>
2024-09-02 17:26:47 +0700

    Time: 53 ms (53.50%), Space: 49.8 MB (10.70%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,26 +4,15 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if(haystack.length < needle.length) return -1
-    
-    let k = 0
-    let firstIndex = 0
-    
-    for(let i = 0; i < haystack.length; i++){
-        if(haystack[i] == needle[0]) {
-            k = i
-            firstIndex = i
-            for(let j = 0; j <= needle.length; j++){
-                if(j == needle.length) {
-                    return firstIndex
-                } else if(haystack[k] != needle[j]) {
-                    break
-                } else {
-                    k++
-                }
-            }
-        }
-    } 
+    if (needle === "") return 0;
         
-    return -1
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let j = 0;
+        while (j < needle.length && haystack[i + j] === needle[j]) {
+            j++;
+        }
+        if (j === needle.length) return i;
+    }
+    
+    return -1;
 };
