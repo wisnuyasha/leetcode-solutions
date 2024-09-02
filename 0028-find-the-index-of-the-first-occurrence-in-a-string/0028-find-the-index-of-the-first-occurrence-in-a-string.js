53bfac9c8d02e6958f79002cd4a3d69994741bd7
Wisnuyasha Faizal <wisnuyeahfaizal@gmail.com>
2024-09-02 17:49:12 +0700

    Time: 34 ms (99.70%), Space: 50.1 MB (8.29%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -5,14 +5,48 @@
  */
 var strStr = function(haystack, needle) {
     if (needle === "") return 0;
-        
-    for (let i = 0; i <= haystack.length - needle.length; i++) {
-        let j = 0;
-        while (j < needle.length && haystack[i + j] === needle[j]) {
+
+    const n = haystack.length;
+    const m = needle.length;
+
+    const lps = new Array(m).fill(0);
+    let len = 0;
+    let i = 1;
+
+    while (i < m) {
+        if (needle[i] === needle[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len !== 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    let j = 0;
+    i = 0; 
+
+    while (i < n) {
+        if (needle[j] === haystack[i]) {
+            i++;
             j++;
         }
-        if (j === needle.length) return i;
+
+        if (j === m) {
+            return i - j;
+        } else if (i < n && needle[j] !== haystack[i]) {
+            if (j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
     }
-    
+
     return -1;
 };
